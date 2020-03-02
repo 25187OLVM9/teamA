0dbbf3da796ad7d5a86404a1257ec066bbf26500
ROOJ <ROOJ@10.150.184.228>
2020-03-02 10:13:22 +0100

    eerste progje

---
+++ src/teamA/voorbeeld/HelloWorld.java
@@ -0,0 +1,15 @@
+package teamA.voorbeeld;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
