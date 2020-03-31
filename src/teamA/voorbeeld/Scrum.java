88e69b681b487b50127400f988a4db6facb6dbc6
Gebruiker <Gebruiker@DESKTOP-2FMMJQ7>
2020-03-31 14:43:53 +0200

    Mijn deel

--- src/teamA/voorbeeld/Scrum.java
+++ src/teamA/voorbeeld/Scrum.java
@@ -15,3 +15,28 @@ public static void Print(ArrayList<Voertuig> lijst) {
         String merk = eenVoertuig.getMerk(); //getMerk pakt het merk variabel uit de superklasse
         String bouw = eenVoertuig.getBouwjaar();
         String kleur = eenVoertuig.getKleur();
+        gegevens.add("Voertuig " + i + ": "); //Doet "Voertuig + i" voor elk voertuig. Elke keer wordt i + 1 gedaan en dus wordt het steeds 1 meer.
+		gegevens.add(merk);
+		gegevens.add(bouw);
+		gegevens.add(kleur);
+		gegevens.add("\n");
+		String keuze = eenVoertuig.getKeuze(); //Waar je kan invullen of je verder of niet verder wil
+		if(keuze.contentEquals("Y")) { //Als Y wordt geantwoord dan wordt dit uitgevoerd
+			i++; //Doet i + 1
+			Print(lijst); //Voert de methode overnieuw uit
+		}
+		else //Wanneer iets anders dan 'Y' wordt geantwoord
+		{	
+			String formateerd = gegevens.toString() //Formateerd de arraylist om het er mooi uit te laten zien
+				    .replace(",", "")  
+				    .replace("[", "")  
+				    .replace("]", "");  
+				              
+			System.out.print(" " + formateerd); //Print de arraylist uit
+		}
+
+	}
+}
+
+
+}
