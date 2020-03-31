6aed6f6d9380d3f35a4f591648f2c092bed6040d
mike- <mike-@DESKTOP-M902NME>
2020-03-31 14:18:49 +0200

    Mijn deel

--- src/teamA/voorbeeld/Voertuig.java
+++ src/teamA/voorbeeld/Voertuig.java
@@ -1,5 +1,36 @@
 package teamA.voorbeeld;
 
-public class Voertuig {
+import java.util.Scanner; //Importeert de scanner
+
+public class Voertuig{
+
+    static String merk;
+    static String bouwjaar;
+    static String kleur;
+    static Scanner s = new Scanner(System.in);
+
+
+    public Voertuig(String merk2, String bouwjaar2, String kleur2) { } //Waar de super naar verwijst
+
+    public String getMerk() { //Waar je het merk invult
+        System.out.println("Voer het merk in: ");
+        String merk = s.nextLine();
+        return merk;
+    }
+    public String getBouwjaar() { //Waar je het Bouwjaar invult
+        System.out.println("Voer het bouwjaar in: ");
+        String bouwjaar = s.nextLine();
+        return bouwjaar;
+    } 
+    public String getKleur() { //Waar je de kleur invult
+        System.out.println("Voer de kleur in: ");
+        String kleur = s.nextLine();
+        return kleur;
+    }
+    public String getKeuze() { //Waar je de keuze invult
+        System.out.println("Wilt u doorgaan? (Y/N)");
+        String keuze = s.nextLine();
+        return keuze;
+    }
 
 }
