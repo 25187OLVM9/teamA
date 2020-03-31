0aaba20f69593f7d2ee8fb84037ef76ae7d2c0c8
royen <royen@LAPTOP-AAFP9UU8>
2020-03-31 14:38:06 +0200

    Mijn deel

--- src/teamA/voorbeeld/Scrum.java
+++ src/teamA/voorbeeld/Scrum.java
@@ -1,5 +1,17 @@
 package teamA.voorbeeld;
 
-public class Scrum {
+public static void voegToe(ArrayList<Voertuig> lijst, Voertuig eenVoertuig) { //Voegt een voertuig toe
+    lijst.add(eenVoertuig);
 
 }
+
+
+static ArrayList<String> gegevens = new ArrayList<String>();
+static int i = 1;
+public static void Print(ArrayList<Voertuig> lijst) {
+
+    for(Voertuig eenVoertuig: lijst) {            //Voert het uit voor hoeveel elementen er in de lijst staat
+
+        String merk = eenVoertuig.getMerk(); //getMerk pakt het merk variabel uit de superklasse
+        String bouw = eenVoertuig.getBouwjaar();
+        String kleur = eenVoertuig.getKleur();
