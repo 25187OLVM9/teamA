91143d631bb3fc13a3e018959758d3bb850c00b8
mike- <mike-@DESKTOP-M902NME>
2020-05-14 11:15:23 +0200

    Quiz 6 en 7

--- src/Quiz7/Subklasse.java
+++ src/Quiz7/Subklasse.java
@@ -1,10 +1,10 @@
-package teamA.Quiz7;
+package Quiz7;
 
 public class Subklasse extends Superklasse
 {
   Subklasse(String naam)
   {
-    super("s"); //Geeft 's' door naar de superklasse
+    super("s"); //Geeft 's' door naar de superklasse.
     System.out.print(naam); //Wordt uitgeprint nadat de superklasse klaar is. Is x vanwege "new Subklasse("x")". Is nu dus sdsx
   }
   public static void main(String[] args)
